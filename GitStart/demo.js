1c31eefbdb6f0588c3b0b822eb194bc45de1eff4
Robins Kumar <robins112002@gmail.com>
2022-11-11 21:06:34 +0530

    first commit

--- GitStart/demo.js
+++ GitStart/demo.js
@@ -9,9 +9,22 @@
 // console.log(document.title);
 
 // console.log(document.getElementById('header-title'));
-var headerTitle = document.getElementById('header-title');
-var header =document.getElementById('main-header');
-// console.log(headerTitle);
-// headerTitle.textContent = 'HELLO';
-// headerTitle.innerText = 'Goodbye'  
- header.style.borderBottom ='solid 3px #000'
+// var headerTitle = document.getElementById('header-title');
+// var header =document.getElementById('main-header');
+// // console.log(headerTitle);
+// // headerTitle.textContent = 'HELLO';
+// // headerTitle.innerText = 'Goodbye'  
+//  header.style.borderBottom ='solid 3px #000'
+
+// GETELEMENTSBYCLASSNAME
+var item =document.getElementsByClassName('list-group-item');
+console.log(item);
+console.log(item[2]);
+
+
+item[2].style.backgroundColor = 'green';
+for(var i=0; i< item.length; i++)
+{
+    item[i].style.fontWeight = 'bold';
+}
+
